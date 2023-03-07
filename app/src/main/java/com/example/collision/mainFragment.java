2ae02e04125d5829506898a548ea0d87a653c688
giracle <clement.girardin@divtec.ch>
2023-03-07 08:50:37 +0100

    Ajout fragment_main.xml

---
+++ app/src/main/java/com/example/collision/mainFragment.java
@@ -0,0 +1,19 @@
+package com.example.collision;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+public class mainFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_main, container, false);
+    }
+}
