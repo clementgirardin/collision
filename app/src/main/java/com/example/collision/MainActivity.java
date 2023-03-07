00ed44929b2f5cbb6e5a9a6c44ca19718c03d272
giracle <clement.girardin@divtec.ch>
2023-03-07 09:34:22 +0100

    Création du premier fragment avec la barre horizontale

--- app/src/main/java/com/example/collision/MainActivity.java
+++ app/src/main/java/com/example/collision/MainActivity.java
@@ -10,5 +10,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Création d'une instance d'infoFragment
+        mainFragment fragmentMain = new mainFragment();
+        // Remplace activity main par le fragment map
+        getSupportFragmentManager().beginTransaction().replace(R.id.container, fragmentMain).commit();
     }
 }
